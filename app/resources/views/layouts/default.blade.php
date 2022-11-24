90776d1f9d63f6b2fa019f78083db060170ceaaa
Martin Halla <thegoodbyte@gmail.com>
2022-11-24 09:24:12 -0500

    added Google analytics

--- app/resources/views/layouts/default.blade.php
+++ app/resources/views/layouts/default.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-B7YRN4155S"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-B7YRN4155S');
+    </script>
  @include('includes.head')
 </head>
 <body class="home page-template page-template-elementor_header_footer page page-id-1799 boldgrid-ppb top-panel-invisible layout-fullwidth blog-default jet-desktop-menu-active elementor-default elementor-template-full-width elementor-kit-1687 elementor-page elementor-page-1799">
