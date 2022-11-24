499a68caa348f62adde039a510c5cedc70c0c61b
Martin Halla <thegoodbyte@gmail.com>
2022-11-23 21:35:08 -0500

    links fixes

--- app/resources/views/pages/home/section-life-is-gift.blade.php
+++ app/resources/views/pages/home/section-life-is-gift.blade.php
@@ -16,12 +16,14 @@
             <h2 class="elementor-heading-title elementor-size-large">IBOGA MEDICÍNA</h2>
             <div class="menu-iboga-container">
 
-                <div class=""><a href="services/worldwide-transport/index.html" class="jet-custom-nav__item-link">Vše o Obřadech</a></div>
-                <div class=""><a href="http://www.ibogaspirit.cz/services/warehousing/" class="jet-custom-nav__item-link">Příprava a Průběžná péče</a></div>
-                <div class=""><a href="services/ground-transport/index.html" class="jet-custom-nav__item-link">Bwiti Tradice</a></div>
-                <div class=""><a href="services/door-to-door-delivery/index.html" class="jet-custom-nav__item-link">Termíny a Ceny</a></div>
-                <div class=""><a href="services/packaging-storage/index.html" class="jet-custom-nav__item-link">Gabonské Pobyty a zasvěcení</a></div>
-                <div class=""><a href="historie-pouzivani/index.html" class="jet-custom-nav__item-link">Historie používání</a></div>
+                <div class=""><a href="/iboga/jak-vam-iboga-muze-pomoci" class="jet-custom-nav__item-link">Jak vám Iboga může pomoci</a></div>
+                <div class=""><a href="/iboga/bwiti-tradice" class="jet-custom-nav__item-link">Bwiti Tradice</a></div>
+                <div class=""><a href="/iboga/iboga-vs-ayahuasca" class="jet-custom-nav__item-link">Iboga vs Ayahuasca</a></div>
+                <div class=""><a href="/obrady/priprava" class="jet-custom-nav__item-link">Příprava a Průběžná péče</a></div>
+
+                <!-- <div class=""><a href="services/door-to-door-delivery/index.html" class="jet-custom-nav__item-link">Termíny a Ceny</a></div> -->
+                <div class=""><a href="/obrady/prvni-ceremonial-a-co-ocekavat" class="jet-custom-nav__item-link">VÁŠ PRVNÍ IBOGA CEREMONIÁL</a></div>
+                <!-- <div class=""><a href="historie-pouzivani/index.html" class="jet-custom-nav__item-link">Historie používání</a></div> -->
             </div>
         </div>
 </section>
