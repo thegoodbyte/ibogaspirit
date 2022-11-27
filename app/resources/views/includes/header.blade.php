1c3df498da0dccf4603879026c4685315a233d82
Martin Halla <thegoodbyte@gmail.com>
2022-11-27 09:18:09 -0500

    Menu fixes

--- app/resources/views/includes/header.blade.php
+++ app/resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <header id="masthead" class="site-header ">
-    <div>
 
-        <section class="row" data-id="9a18821" data-element_type="section" data-settings="{&quot;jet_parallax_layout_list&quot;:[],&quot;background_background&quot;:&quot;classic&quot;}">
+
+        <section class="row">
             <div class="elementor-container elementor-column-gap-no">
                 <div class="" data-id="713a3ea" data-element_type="column">
                     <div class="elementor-widget-wrap elementor-element-populated">
@@ -14,5 +14,5 @@
 {{--        <section>--}}
 {{--            <div class = "header-bottom-row">2</div>--}}
 {{--        </section>--}}
-    </div>
+
 </header>
