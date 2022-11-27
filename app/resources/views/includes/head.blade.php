cbafb316fb5ecfd77bbfd7f7e8f24702eb3718d0
Martin Halla <thegoodbyte@gmail.com>
2022-11-26 21:18:53 -0500

    PARALLAX home - html section fix

--- app/resources/views/includes/head.blade.php
+++ app/resources/views/includes/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>TRADIČNÍ  IBOGA  OBŘADY   V AFRICKÉ  TRADICI MISSOKO BWITI - Ceska Republika</title>
+<title>TRADIČNÍ  IBOGA  OBŘADY   V AFRICKÉ  TRADICI MISSOKO BWITI - Česká republika</title>
 <link rel="stylesheet" href="/css/app.css">
 
 
--- app/resources/views/pages/home/section-parallax.blade.php
+++ app/resources/views/pages/home/section-parallax.blade.php
@@ -1,28 +1,22 @@
-<section id = "home-parallax" class="elementor-section elementor-top-section elementor-element elementor-element-60b33f5 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="60b33f5" data-element_type="section" data-settings="{&quot;jet_parallax_layout_list&quot;:[],&quot;background_background&quot;:&quot;classic&quot;}">
-    <div class="elementor-container elementor-column-gap-no">
-        <div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-24525a7 elementor-hidden-tablet elementor-hidden-mobile" data-id="24525a7" data-element_type="column">
-            <div class="elementor-widget-wrap">
+<section id = "home-parallax" class="row">
+
+        <div class="row">
+            <div class = "col-xl-6">
+
+            </div>
+
+            <div class = "col-xl-6">
+                <h2 class="">TRADIČNÍ BWITI LÉČENÍ A DUCHOVNÍ UČENÍ</h2>
+                <p>
+                Podle tradice,  když se Iboga poprvé objevila,  rozhodl se být s Bwiti lidmi. Iboga respektoval jejich
+                způsob života, a tak když se lidé začali ptát,
+                odkud přišli a za co mohou být tolik vděční, Iboga se představil a řekl: „Řekněte svým lidem, že až
+                budou mít otázky, at si me vezmou mě a já jim ukažu."
+
+                A v tomto příběhu je toho mnohem víc...
+
+                </p>
             </div>
         </div>
-        <div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-3eeb7cf animated fadeIn" data-id="3eeb7cf" data-element_type="column" data-settings="{&quot;animation&quot;:&quot;fadeIn&quot;}">
-            <div class="elementor-widget-wrap elementor-element-populated">
-                <div class="elementor-element elementor-element-a7031f0 elementor-widget__width-initial elementor-widget-mobile__width-inherit elementor-widget elementor-widget-heading is-mac" data-id="a7031f0" data-element_type="widget" data-widget_type="heading.default">
-                    <div class="elementor-widget-container">
-                        <h2 class="elementor-heading-title elementor-size-default"><div data-pm-slice="1 1 []" data-en-clipboard="true">TRADIČNÍ BWITI LÉČENÍ A DUCHOVNÍ UČENÍ</div>
-                        </h2>		</div>
-                </div>
-                <p class="elementor-element elementor-element-69e8cef elementor-widget__width-initial elementor-widget-mobile__width-inherit elementor-widget elementor-widget-text-editor is-mac" data-id="69e8cef" data-element_type="widget" data-widget_type="text-editor.default">
-                    <p>
-                    Podle tradice,  když se Iboga poprvé objevila,  rozhodl se být s Bwiti lidmi. Iboga respektoval jejich
-                    způsob života, a tak když se lidé začali ptát,
-                    odkud přišli a za co mohou být tolik vděční, Iboga se představil a řekl: „Řekněte svým lidem, že až
-                    budou mít otázky, at si me vezmou mě a já jim ukažu."
 
-                    A v tomto příběhu je toho mnohem víc...
-
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
 </section>
