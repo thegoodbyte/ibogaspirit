b4939ef40c2317175c88141477aa57062649acde
Martin Halla <thegoodbyte@gmail.com>
2022-11-26 19:31:28 -0500

    REMOVAL HEAD 4

--- app/resources/views/pages/home/section-kdo-jsme.blade.php
+++ app/resources/views/pages/home/section-kdo-jsme.blade.php
@@ -1,125 +1,78 @@
-<section id = "home-kdo-jsme" class="elementor-section elementor-top-section elementor-element elementor-element-898f81e elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="898f81e" data-element_type="section" data-settings="{&quot;jet_parallax_layout_list&quot;:[],&quot;background_background&quot;:&quot;classic&quot;}">
-    <div class="elementor-container elementor-column-gap-no">
+<section id = "home-kdo-jsme" class="row">
+    <div class="row">
+
+
         <div class="col-lg-4 box-kdo-jsme">
             <div class="row">
-                <div class="elementor-element elementor-element-1e52df5 elementor-widget__width-initial elementor-widget elementor-widget-heading is-mac" data-id="1e52df5" data-element_type="widget" data-widget_type="heading.default">
-                    <div class="elementor-widget-container">
+                <div class="row">
+                    <div class="row">
                         <h2 class="elementor-heading-title elementor-size-default">CO JE IBOGA</h2>
                     </div>
                 </div>
-                <div class="elementor-element elementor-element-ac5a4af elementor-widget__width-initial elementor-widget elementor-widget-text-editor is-mac" data-id="ac5a4af" data-element_type="widget" data-widget_type="text-editor.default">
-                    <div class="elementor-widget-container text-uppercase">
-                        Iboga (Tabernathe Iboga) je vytrvalý keř pocházející z rovníkové středozápadní Afriky. Kořenová kůra byla používána jako přírodní rostlinná medicína po tisíce a tisíce let domorodými obyvateli regionu.
+                <div class="row">
+                    <div class="row text-uppercase p-3">
+                        Iboga (Tabernathe Iboga) je vytrvalý keř pocházející z rovníkové středozápadní Afriky. Kořenová kůra byla používána
+                        jako přírodní rostlinná medicína po tisíce a tisíce let domorodými obyvateli regionu.
                         Je považována za posvátnou svátost těch, kteří praktikují tradici známou jako Bwiti.
                     </div>
                 </div>
-                <div class="elementor-element elementor-element-de9b744 elementor-widget__width-initial elementor-widget elementor-widget-text-editor is-mac" data-id="de9b744" data-element_type="widget" data-widget_type="text-editor.default">
-                    <div class="elementor-widget-container">
+                <div class="row">
+                    <div class="row">
                         Iboga se používá v různých obřadních ...
                     </div>
                 </div>
 
             </div>
-
-            <div class="row row-show-more-button" data-id="2ed10eb" data-element_type="widget" data-widget_type="jet-button.default">
-                <div class="elementor-widget-container">
-                    <div class="elementor-jet-button jet-elements">
-                        <div class="jet-button__container">
-                            <a class="jet-button__instance jet-button__instance--icon- hover-effect-0" href="/obrady/terminy">
-                                <div class="show-more-button jet-button__plane jet-button__plane-normal"></div>
-                                <div class="jet-button__plane jet-button__plane-hover"></div>
-                                <div class="jet-button__state jet-button__state-normal">
-                                    <span class="jet-button__label">Zobrazit více</span>
-                                </div>
-                                <div class="jet-button__state jet-button__state-hover">
-                                    <span class="jet-button__label">Zobrazit více ></span>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
         </div>
+
+
+
+
         <div class="col-lg-4 box-kdo-jsme">
             <div class="">
                 <div class="">
-                    <div class="elementor-widget-container">
+                    <div class="row">
                         <h2 class="elementor-heading-title elementor-size-default"><span style="text-align: inherit;">O MNE</span></h2>		</div>
                 </div>
-                <div class="elementor-element elementor-element-101ab0d elementor-widget__width-initial elementor-widget elementor-widget-text-editor is-mac" data-id="101ab0d" data-element_type="widget" data-widget_type="text-editor.default">
-                    <div class="elementor-widget-container uppercase">
-                        VItejte - Ja jsem Martin and jsem Nganga - spiritualni lecitel a tradicni Bwiti Iboga facilitator a poskytovatel.
-                        Studoval jsem bezpecne poskytovaní Ibogy medicíny a Bwiti uceni a lecení v Gabonu,  stredozapadni Africe, u samana 10-te generace Moughendy Mikaly.
+                <div class="row">
+                    <div class="elementor-widget-container uppercase p-3">
+                        VItejte - jmenuji se Martin and jsem Nganga - spirituálni lécitel a tradicni Bwiti Iboga facilitátor a poskytovatel.
+                        Studoval jsem bezpecné poskytování Ibogy medicíny a Bwiti ucení a lécení v Gabonu,  stredozápadni Africe, u samana 10-té generace Moughendy Mikaly.
                         Ucil jsem se tradicní  tisíce-letou praxi v  Missoko Bwiti.
                     </div>
                 </div>
-                <div class="elementor-element elementor-element-1e3abfe elementor-widget__width-initial elementor-widget elementor-widget-text-editor is-mac" data-id="1e3abfe" data-element_type="widget" data-widget_type="text-editor.default">
-                    <div class="elementor-widget-container">
-                        <p>Pusobim mezinarodne ... momentalne v Mexickem Kankunu v <a href = "www.bwitilife.com">Bwitilife.com</a></p>						</div>
+                <div class="row">
+                    <div class="row">
+                        <p>Pusobím mezinarodne ... momentálně v Mexickém Kankunu v <a href = "www.bwitilife.com">Bwitilife.com</a></p>						</div>
                 </div>
 
-            </div>
-
-            <div class="row row-show-more-button" data-id="2ed10eb" data-element_type="widget" data-widget_type="jet-button.default">
-                <div class="elementor-widget-container">
-                    <div class="elementor-jet-button jet-elements"><div class="jet-button__container">
-                            <a class="jet-button__instance jet-button__instance--icon- hover-effect-0" href="/obrady/terminy">
-                                <div class="show-more-button jet-button__plane jet-button__plane-normal"></div>
-                                <div class="jet-button__plane jet-button__plane-hover"></div>
-                                <div class="jet-button__state jet-button__state-normal">
-                                    <span class="jet-button__label">Zobrazit více</span>
-                                </div>
-                                <div class="jet-button__state jet-button__state-hover">
-                                    <span class="jet-button__label">Zobrazit více</span>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
-        <div class="col-lg-4 box-kdo-jsme ">
+
+
+
+        <div class="col-lg-4 box-kdo-jsme">
             <div class="row">
-                <div class="" data-id="5c461c3" data-element_type="widget" data-widget_type="heading.default">
-                    <div class="elementor-widget-container">
-                        <h2 class="font-just-me">Termíny</h2>		</div>
+                <div class="row">
+                    <div class="row">
+                        <h2 class="font-just-me">Termíny</h2>
+                    </div>
                 </div>
-                <div class="elementor-element elementor-element-33ddd86 elementor-widget__width-initial elementor-widget elementor-widget-text-editor is-mac" data-id="33ddd86" data-element_type="widget" data-widget_type="text-editor.default">
-                    <div class="elementor-widget-container">
+                <div class="row">
+                    <div class="row p-3">
                         Tradiční Missoko Bwiti Iboga obřady poskytujeme 4x ročně nebo i častěji, dle zájmu.<br />
                         Naše další dva retreaty se budou konat:<br /><br />
                     </div>
                 </div>
-                <div class="elementor-element elementor-element-46f3725 elementor-widget__width-initial elementor-widget elementor-widget-text-editor is-mac" data-id="46f3725" data-element_type="widget" data-widget_type="text-editor.default">
-                    <div class="elementor-widget-container">
+                <div class="row">
+                    <div class="row">
                         <strong>17 - 24 března 2023</strong><br />
-                        Blatná - Střední Čechy<br /><br />
+                        Pecka, Jičíny<br /><br />
                     </div>
 
-                    <div class="elementor-widget-container">
-                        <strong>17 - 24 června 2023.</strong><br />
-                        Sýkořice - Berounka - Křivoklátsko
-                    </div>
-                </div>
-
-
-            </div>
-
-            <div class="row row-show-more-button" data-id="2ed10eb" data-element_type="widget" data-widget_type="jet-button.default">
-                <div class="elementor-widget-container">
-                    <div class="elementor-jet-button jet-elements"><div class="jet-button__container">
-                            <a class="jet-button__instance jet-button__instance--icon- hover-effect-0" href="/obrady/terminy-a-ceny">
-                                <div class="show-more-button jet-button__plane jet-button__plane-normal"></div>
-                                <div class="jet-button__plane jet-button__plane-hover"></div>
-                                <div class="jet-button__state jet-button__state-normal">
-                                    <span class="jet-button__label">Zobrazit více</span>
-                                </div>
-                                <div class="jet-button__state jet-button__state-hover">
-                                    <span class="jet-button__label">Zobrazit více</span>
-                                </div>
-                            </a>
-                        </div>
+                    <div class="row">
+                        <strong>17 - 23 června 2023.</strong><br />
+                        Vidochov, Královéhradecký kraj
                     </div>
                 </div>
             </div>
