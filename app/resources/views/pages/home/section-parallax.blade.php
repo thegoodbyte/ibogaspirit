6972f09e126fbc43a5cd6f75ade8f565863db8c6
Martin Halla <thegoodbyte@gmail.com>
2022-11-27 15:48:27 -0500

    Added What is Iboga page

--- app/resources/views/pages/home/section-parallax.blade.php
+++ app/resources/views/pages/home/section-parallax.blade.php
@@ -1,22 +1,18 @@
 <section id = "home-parallax" class="row">
-
-        <div class="row">
-            <div class = "col-xl-6 col-xs-12">
-
-            </div>
-
-            <div class = "col-xl-6 col-xs-12">
-                <h2 class="">TRADIČNÍ BWITI LÉČENÍ A DUCHOVNÍ UČENÍ</h2>
-                <p>
-                    Podle tradice,  když se Iboga poprvé objevila,  rozhodl se být s Bwiti lidmi. Iboga respektoval jejich
-                    způsob života, a tak když se lidé začali ptát,
-                    odkud přišli a za co mohou být tolik vděční, Iboga se představil a řekl: „Řekněte svým lidem, že až
-                    budou mít otázky, at si me vezmou mě a já jim ukažu."
-
-                    A v tomto příběhu je toho mnohem víc...
-
-                </p>
-            </div>
+        <div class = "col-xl-6 col-xs-12">
+            &nbsp;
         </div>
 
+        <div class = "col-xl-6 col-xs-12">
+            <h2 class="">TRADIČNÍ BWITI LÉČENÍ A DUCHOVNÍ UČENÍ</h2>
+            <p>
+                Podle tradice,  když se Iboga poprvé objevila,  rozhodl se být s Bwiti lidmi. Iboga respektoval jejich
+                způsob života, a tak když se lidé začali ptát,
+                odkud přišli a za co mohou být tolik vděční, Iboga se představil a řekl: „Řekněte svým lidem, že až
+                budou mít otázky, at si me vezmou mě a já jim ukažu."
+
+                A v tomto příběhu je toho mnohem víc...
+
+            </p>
+        </div>
 </section>
