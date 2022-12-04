aaf5760bb7800111f88f6c9ed9475794ffe64280
Martin Halla <thegoodbyte@gmail.com>
2022-12-04 12:00:01 -0500

    INTL - ngangacz

---
+++ app/resources/lang/cz/pages/about-us/nganga.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    'content' => [
+        'title' => 'Nganga - Duchovní léčitel',
+        'par_1'=> 'Vitejte - já jsem Martin - spirituální léčitel neboli "<strong>Nganga</strong>" - tradiční Missoko Bwiti Iboga facilitátor.
+                Studoval jsem tradiční
+                léčení Ibogou u šamana 10-té generace přímo v Gabonu ve středozápadní Africe. Pocházím ze Západních Čech - z Rokycanska -
+                ale již přes 20 let bydlím v Severní Americe a praktuji převážně v Mexiku.
+                Nyní nabízím Iboga obřady také u nás v Čechách.',
+        'par_2'=> 'Zamiloval jsem se do tradice Bwiti a Ibogy medicíny protože nás učí naši vnitřní pravdu a
+                jak krásný,  jednoduchý a nekomplikovaný  náš život je a může být.
+                Rostlina Iboga je zároveň léčitelem a učitelem.
+                Iboga mi pomohla, když jsem v životě procházel traumatickým obdobím.
+                Před tímto obdobím jsem býval velmi úspěšný a šťastný, ale pak mě traumatická událost poslala do
+                falešných domněnek a představ, úzkostí, strachu a panických záchvatů.
+                Hodně jsem na sobě pracoval - autosugesce, ruzné  toltécké šamanské praktiky, kurzy od Tonyho Robinse,
+                zákon přitažlivosti, věstkyně,  každodenní meditace atd. Pomalu jsem pokračoval
+                 ale nikam jsem se nedostával.',
+        'par_3'=> 'Zamiloval jsem se do tradice Bwiti a Ibogy medicíny protože nás učí naši vnitřní pravdu a
+                jak krásný,  jednoduchý a nekomplikovaný  náš život je a může být.
+                Rostlina Iboga je zároveň léčitelem a učitelem.
+                Iboga mi pomohla, když jsem v životě procházel traumatickým obdobím.
+                Před tímto obdobím jsem býval velmi úspěšný a šťastný, ale pak mě traumatická událost poslala do
+                falešných domněnek a představ, úzkostí, strachu a panických záchvatů.
+                Hodně jsem na sobě pracoval - autosugesce, ruzné  toltécké šamanské praktiky, kurzy od Tonyho Robinse,
+                zákon přitažlivosti, věstkyně,  každodenní meditace atd. Pomalu jsem pokračoval
+                 ale nikam jsem se nedostával.',
+        'par_4'=> 'Po týdenním pobytu (a dvouch obřadech) to bylo, jako bych to byl nový já  - se všemi svými předchozími vlastnostmi, ale tentokrát lepšími..
+               Navíc jsem se cítil a vypadal jako omlazený.
+                Mé srdce bylo znovu otevřené, nejen k tomu milovat druhé , ale také milovat sám sebe.
+                Nemohl jsem uvěřit, že existuje tak úžasné a jednoduché řešení pro různé komplikované problémy a podmínky',
+        'par_5'=> 'Zamiloval jsem si Ibogu a rozhodl se že pojedu přimo do Afriky do Gabonu osobně poděkovat této vzácné a
+                úžasné rostlině.
+                K mému obdivu  jsem byl nečekaně zasvěcen do Bwiti tradice a hned poznal, že já a tato medicína máme vzácný vztah.',
+        'par_6'=> 'Po tolika úžasných zkušenostech jsem chtěl pomáhat léčit i ostatní, a tak jsem se rozhodl učit se od šamana, který vycvičil i mého vlastního léčitele.
+                 Vrátil jsem se do Gabonu  a trénoval s šamanem , Moughendou Mikalou, šamanem 10-té generace v Missoko tradici Bwiti.',
+        'par_7'=> 'Jsem nadšený, že jsem objevil něco tak mocného a že mám příležitost být užitečný, sdílet to s druhými a podporovat ostatní v léčení.
+                Když se lidé uzdraví a najdou ve svých životech radost, má to dominový efekt na lidskou  společnost a také budoucí generace dětí.',
+        'quote'=> 'Jsem odhodlán usnadnit vaše uzdravení tím nejlepším možným způsobem který jsem se naučil přímo v Africe od Moughendy.
+                Jsem tu pro vás, i když jste možná jen zvědaví.',
+        'par_8'=> 'Momentálně nejvíce působím v Mexickém Kankůnu v centru "Bwiti Life" (http://www.bwitilife.com) u svého Bwiti bratra a Ibogy facilitátora Michaela Cassidyho.',
+        'par_9'=> 'Veškery pobyt a obřady jsem schopný poskytnout v rodné češtině, angličtině či polštině.
+                Obřad mohu též poskytnout v němčině s pomocí svého Bwiti bratra a kamaráda a Ibogy facilitátora - Nicka Sunshilda.',
+        'par_10'=> 'Kdykoli mě prosím kontaktujte a napište mi, i jen s jakoukoliv otázkou. Bude mi potěšením se s vámi setkat a pomoci vám, jak jen budu moci!',
+
+    ]
+];
--- app/resources/views/pages/onas/nganga.blade.php
+++ app/resources/views/pages/onas/nganga.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     <div class = "row">
-        <h2 class = "font-just-me font-size-60 text-center color-iscz-red">Nganga - Duchovní léčitel</h2>
+        <h2 class = "font-just-me font-size-60 text-center color-iscz-red"><?php echo __('pages/about-us/nganga.content.title'); ?></h2>
     </div>
 
 
@@ -12,34 +12,17 @@
         </div>
 
         <div class = "col-xl-7">
-            <p> Vitejte - já jsem Martin - spirituální léčitel neboli "<strong>Nganga</strong>" - tradiční Missoko Bwiti Iboga facilitátor.
-                Studoval jsem tradiční
-                léčení Ibogou u šamana 10-té generace přímo v Gabonu ve středozápadní Africe. Pocházím ze Západních Čech - z Rokycanska - ale již přes 20 let bydlím v Severní Americe a praktuji převážně v Mexiku.
-                Nyní nabízím Iboga obřady také u nás v Čechách.</p>
+            <p><?php echo __('pages/about-us/nganga.content.par_1'); ?></p>
             <p>
-                Zamiloval jsem se do tradice Bwiti a Ibogy medicíny protože nás učí naši vnitřní pravdu a
-                jak krásný,  jednoduchý a nekomplikovaný  náš život je a může být.
-                Rostlina Iboga je zároveň léčitelem a učitelem.
-                Iboga mi pomohla, když jsem v životě procházel traumatickým obdobím.
-                Před tímto obdobím jsem býval velmi úspěšný a šťastný, ale pak mě traumatická událost poslala do
-                falešných domněnek a představ, úzkostí, strachu a panických záchvatů.
-                Hodně jsem na sobě pracoval - autosugesce, ruzné  toltécké šamanské praktiky, kurzy od Tonyho Robinse,
-                zákon přitažlivosti, věstkyně,  každodenní meditace atd. Pomalu jsem pokračoval
-                 ale nikam jsem se nedostával.
+                <?php echo __('pages/about-us/nganga.content.par_2'); ?>
             </p>
 
             <p>
-                O Iboze medicíně jsem se prvně dozvěděl od polské věštkyně z Chikága.
-                Pověděla mi "Jeď na Ibogu a všechny tvoje problémy budou vyřešeny".
-                Zdálo se mi to jako nějaká fráze a nevěřil jsem tomu. Trvalo mi to 7 let než jsem učinil ten první krok a
-                vydal se za šamanským Bwiti léčením do blízké Kostariky.
+                <?php echo __('pages/about-us/nganga.content.par_3'); ?>
             </p>
 
             <p>
-               Po týdenním pobytu (a dvouch obřadech) to bylo, jako bych to byl nový já  - se všemi svými předchozími vlastnostmi, ale tentokrát lepšími..
-               Navíc jsem se cítil a vypadal jako omlazený.
-                Mé srdce bylo znovu otevřené, nejen k tomu milovat druhé , ale také milovat sám sebe.
-                Nemohl jsem uvěřit, že existuje tak úžasné a jednoduché řešení pro různé komplikované problémy a podmínky.
+                <?php echo __('pages/about-us/nganga.content.par_4'); ?>
             </p>
 
             <!--
@@ -65,9 +48,7 @@
     <div class = "row">
         <div class = "col-xl-12">
             <p>
-                Zamiloval jsem si Ibogu a rozhodl se že pojedu přimo do Afriky do Gabonu osobně poděkovat této vzácné a
-                úžasné rostlině. <!--Psal se rok 2021.-->
-                K mému obdivu  jsem byl nečekaně zasvěcen do Bwiti tradice a hned poznal, že já a tato medicína máme vzácný vztah.
+                <?php echo __('pages/about-us/nganga.content.par_5'); ?>
             </p>
         </div>
     </div>
@@ -77,18 +58,15 @@
         <div class = "col-xl-5">
 
             <p>
-                Po tolika úžasných zkušenostech jsem chtěl pomáhat léčit i ostatní, a tak jsem se rozhodl učit se od šamana, který vycvičil i mého vlastního léčitele.
-                 Vrátil jsem se do Gabonu  a trénoval s šamanem , Moughendou Mikalou, šamanem 10-té generace v Missoko tradici Bwiti.
+                <?php echo __('pages/about-us/nganga.content.par_6'); ?>
             </p>
 
             <p>
-                Jsem nadšený, že jsem objevil něco tak mocného a že mám příležitost být užitečný, sdílet to s druhými a podporovat ostatní v léčení.
-                Když se lidé uzdraví a najdou ve svých životech radost, má to dominový efekt na lidskou  společnost a také budoucí generace dětí.
+                <?php echo __('pages/about-us/nganga.content.par_7'); ?>
             </p>
 
             <blockquote>
-                Jsem odhodlán usnadnit vaše uzdravení tím nejlepším možným způsobem který jsem se naučil přímo v Africe od Moughendy.
-                Jsem tu pro vás, i když jste možná jen zvědaví.
+                <?php echo __('pages/about-us/nganga.content.quote'); ?>
             </blockquote>
         </div>
 
@@ -103,17 +81,14 @@
 
 
             <p>
-                Momentálně nejvíce působím v Mexickém Kankůnu v centru "Bwiti Life" (<a href = "http://www.bwitilife.com">http://www.bwitilife.com</a>)
-                u svého Bwiti bratra a Ibogy facilitátora Michaela Cassidyho.
+                <?php echo __('pages/about-us/nganga.content.par_8'); ?>
             </p>
 
             <p>
-                Veškery pobyt a obřady jsem schopný poskytnout v rodné češtině, angličtině či polštině. Obřad mohu též poskytnout v němčině s pomocí svého Bwiti bratra a kamaráda a
-                Ibogy facilitátora  - Nicka Sunshilda.
+                <?php echo __('pages/about-us/nganga.content.par_9'); ?>
             </p>
 
-            <p>Kdykoli mě prosím kontaktujte a napište mi, i jen s jakoukoliv otázkou.
-            Bude mi potěšením se s vámi setkat a pomoci vám, jak jen budu moci!</p>
+            <p><?php echo __('pages/about-us/nganga.content.par_10'); ?></p>
 
             <p><img src = "/images/home/onas/my-signture.png"></p>
         </div>
