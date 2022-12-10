c9f87e1f3c7c27ed4cc992023eb7386da2ff435f
Martin Halla <thegoodbyte@gmail.com>
2022-12-09 23:33:31 -0500

    added more home pars

--- app/resources/views/pages/home/index.blade.php
+++ app/resources/views/pages/home/index.blade.php
@@ -10,10 +10,22 @@
         <p class = "text-uppercase">
             Šamanské léčení a učení k léčbě jakékoliv mentální, duševní nebo fyzické nemoci. Vydejte se na cestu sebepoznání
             a osvoboďte se z pout ega a mysli. Utěšte svou duši a žijte šťastný život. Jeden týdenní pobyt s dvěma obřady Ibogy může změnit
-            váš život k lepšímu. Už žádné nešťastné smutné nebo popletené vy - jen nový a krásný život před vámi, A UŽívání si ho naplno.<br />
-            IBOGA MEDICÍNA  MŮŽE VYLÉČIT KAŽDÉ DUŠEVNÍ TRAUMA
-            TÍM ŽE NÁS PŘIVEDE KE KOŘENŮM PROBLÉMU A UKÁŽE NÁM, JAK SE S NÍM SMÍŘIT A ZÁROVEŇ NÁS VYLÉČÍ.
-            JE TO SKUTEČNÝ ZÁZRAK – ZNOVUROZENÍ
+            váš život k lepšímu. Už žádné nešťastné smutné nebo popletené vy - jen nový a krásný život před vámi, A užívání si ho naplno.<br />
+        </p>
+
+        <p>
+            Iboga je hluboce uzemňující medicína, který používá pravdu k léčení. Jejím cílem je vás přivést  zpět
+            ke svému původnímu pravému já a znovu vás spojí s vaší duší. Dělá to tak, že nejprve vyčistí (detoxikuje)
+            vše, co může, co fyzicky a duševně k vám nepatří . To je často zažíváno jako náhodné myšlenky,
+            obrázky a takové proudění, které všechno vyčistí, je podobná k defragmentaci počítače.
+        </p>
+
+        <p>Iboga vám také ukáže jak funguje vaše mysl a odhalí vzorce myšlení, které jste možná pochytili
+            během vašeho života. Vzorce které nejsou pravdivé nebo zdravé. Poté, co Iboga obnoví mír a pokoj ve vaší mysli,
+            odhalí vám původ vašeho utrpení a   učí vás dále tím, že vám ukazuje pravdu. Zkrátka vám to
+            pomáhá uvolnit vzorce, vzpomínky a traumata a osvobodí vás od nich – abyste mohli být tím svým pravým já. Po ceremoniálu,
+            Iboga vám nadále pomáhá zlepšováním všímavosti a podporou neuroplasticity, abyste mohli vytvořit nový,
+            zdravější život s lehkostí.
         </p>
     </div>
 </div>
