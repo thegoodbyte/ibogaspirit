2c21866cce3063c9f057455623727d40cc684bab
Martin Halla <thegoodbyte@gmail.com>
2023-01-04 16:11:47 -0500

    Updated dates and locations

--- app/resources/views/pages/ceremonies/dates.blade.php
+++ app/resources/views/pages/ceremonies/dates.blade.php
@@ -6,28 +6,29 @@
     <div class = "row">
         <div class = "col-xl-12">
             <p>Pobyty (retreaty) Ibogy v ČR nabízím čtyřikrát ročně, někdy i více dle zájmu.
-                Jedná se o malé skupiny 2 - 5 účastníků. Místo léčebneho pobytu
+                Jedná se o malé skupiny 2 - 5 účastníků. Místo léčebného pobytu
                 je obvykle ve Východních Čechách</p>
 
             <p>
-                Běžná celosvětová cena je $3.500 $ za týdenní pobyt.
+                Běžná celosvětová cena je $3.500 za týdenní pobyt.
                 V současné době nabízím zvýhodněné pobyty. Jako poděkování vlastní zemi - čeští rodáci platí jen polovinu...
             </p>
         </div>
 
         <div class = "row">
             <div class = "col-xl-4 col-sm-12">
+
                 <div class = "row retreat-info">
-                    <h3>17 - 24 března 2023</h3>
-                    <h4 class = "text-justify"> Pecka, Jičín</h4>
-                    <h5 class = "text-justify">Královohradecko</h5>
+                    <h3>25 března  - 1 dubna 2023</h3>
+                    <h4 class = "text-justify"> Železná Ruda, Šumava</h4>
+                    <h5 class = "text-justify">Západočeský Kraj</h5>
 
                     <div class = "retreat-image  text-justify"">
                         <img src = "/images/retreats/2023/march.png" width = "300"/>
                     </div>
 
                     <div class = "free-spots text-justify">
-                        Dvě volná místa
+                        Tři volná místa
                     </div>
 
                     <div class = "retreat-price">
@@ -44,8 +45,8 @@
 
             <div class = "col-xl-4 col-sm-12">
                 <div class = "row retreat-info">
-                    <h3>16 - 23 června 2023</h3>
-                    <h4> Vidochov, Královéhradecký kraj</h4>
+                    <h3>17 - 24 června 2023</h3>
+                    <h4> Svoboda nad Úpou, Královéhradecký kraj</h4>
                     <h5>Královéhradecký Kraj</h5>
 
                     <div class = "retreat-image">
